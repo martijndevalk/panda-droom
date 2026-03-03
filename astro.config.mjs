2958312b1b4a3157e47e094bbdde978462a8d67c
Martijn de Valk <mdevalk@kaliber.net>
2026-03-03 17:47:02 +0100

    Add deployment configurations
    - Specify site URL and base path for deployment
    - Update configuration to reflect new structure

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,5 +4,7 @@ import tailwind from '@astrojs/tailwind';
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://martijndevalk.github.io',
+  base: '/panda-droom',
   integrations: [react(), tailwind()]
 });
